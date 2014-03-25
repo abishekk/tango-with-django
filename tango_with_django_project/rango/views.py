dd6712ad03ea021276a68406259dfcdf43d252d5
Abishek K <abishekk@abis-mbp.com>
2014-03-25 13:39:02 +0530

    change index view to use templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,18 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world! <br/> "
-                        "<a href='/rango/about'>About</a>")
+    # obtain the request context
+    context = RequestContext(request)
+    
+    # construct template variables as a dictionary
+    context_dict = {'boldmessage' : "I am the bold message in the context"}
+
+    # render it using the django shortcut
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
     return HttpResponse("Rango says: Here is the about page."
