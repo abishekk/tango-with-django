d3c3c3294b06ff6250eb54938a19e00b9bf77109
Abishek K <abishekk@abis-mbp.com>
2014-03-25 13:14:30 +0530

    adding the rango app's first views and url patterns

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hello world! <br/> "
+                        "<a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says: Here is the about page."
+                        "<br/> <a href='/rango/'>Home</a>")
