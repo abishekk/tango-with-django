41d156a8cfbc2409aac6903681521badd0bedb22
Abishek K <abishekk@abis-mbp.com>
2014-03-27 11:50:46 +0530

    url for admin interface

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')),
     url(r'media/(?P<path>.*)', 'django.views.static.serve', {
         'document_root' : settings.MEDIA_ROOT }),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 # urlpatterns += patterns('django.views.static',
