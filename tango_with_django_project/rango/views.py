df4623b7ee4772dd4c0e6d81019131681fedc081
Abishek K <abishekk@abis-mbp.com>
2014-03-27 13:29:39 +0530

    Add Category view to contain page list, with template and url adds. Also add index view to store the url

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     # obtain the request context
@@ -13,6 +14,10 @@ def index(request):
     # obtain the top 5 categories
     category_list = Category.objects.order_by('-likes')[:5]
     # construct template variables as a dictionary
+
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
     context_dict = {'categories' : category_list }
 
     # render it using the django shortcut
@@ -27,3 +32,25 @@ def about(request):
 
     # render it using the django shortcut
     return render_to_response('rango/about.html', context_dict, context)
+
+def category(request, category_name_url):
+    # Request context from the request passed to us
+    context = RequestContext(request)
+
+    # Change underscores in the category name to spaces
+    category_name = category_name_url.replace('_', ' ')
+
+    context_dict = {'category_name' : category_name}
+
+    try:
+        category = Category.objects.get(name = category_name)
+
+        pages = Page.objects.filter(category = category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
