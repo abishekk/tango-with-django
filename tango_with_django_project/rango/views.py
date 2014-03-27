9b24c0bd6bdfe9f2d85a35c76f5f91b83056cda4
Abishek K <abishekk@abis-mbp.com>
2014-03-27 12:57:53 +0530

    index view now contains a list of categories

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,12 +4,16 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
+from rango.models import Category
+
 def index(request):
     # obtain the request context
     context = RequestContext(request)
 
+    # obtain the top 5 categories
+    category_list = Category.objects.order_by('-likes')[:5]
     # construct template variables as a dictionary
-    context_dict = {'boldmessage' : "I am the bold message in the context"}
+    context_dict = {'categories' : category_list }
 
     # render it using the django shortcut
     return render_to_response('rango/index.html', context_dict, context)
